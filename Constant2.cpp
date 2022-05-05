908500975c5b8b11a4733280df73424fe06502ee
RoshanKalek7 <99859897+RoshanKalek7@users.noreply.github.com>
2022-05-05 10:08:28 +0600

    Add files via upload

---
+++ Constant2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int i;
+        const int j;
+
+        Demo() : j(20)      // Member initialization
+        {
+            i = 10;
+			i++;
+			//j++;    // error
+            // j = 20;
+        }
+};
+
+int main()
+{
+   Demo obj;
+   cout<<obj.i<<"\n";
+   cout<<obj.j<<"\n";
+
+    return 0;
+}
